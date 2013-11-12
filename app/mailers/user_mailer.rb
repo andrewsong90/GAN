a4dd728da3f4b0c8d46e4f469dddfc7602472f92
Andrew Song <andrewsong90@gmail.com>
2013-11-11 21:23:09 -0500

    no errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,9 @@ class UserMailer < ActionMailer::Base
 	mail(to: @user.email, subject:"[GAN] Welcome to the GAN - Gann Alumni Networks!!!")	
   end
 
+  def opportunity_email(user)
+  end
+
   def submission_email(user,application)
   	@user, @application, @opportunity =user, application, application.opportunity
   	mail(to: @opportunity.user.email, cc: @user.email, subject:"[GAN] Application has been submitted")
