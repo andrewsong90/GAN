b7fdcf7282d27a408f2889f2df3535967304dc52
Andrew Song <andrewsong90@gmail.com>
2013-11-20 00:47:56 -0500

    COMMIT

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,9 @@ Bundler.require(:default, Rails.env)
 
 #ENV.update YAML.load(File.read(File.expand_path('../application.yml',__FILE__)))
 
-config.before_configuration do 
-
-    env_file=File.join(Rails.root,'config','application.yml')
-    YAML.load(File.open(env_file)).each do |key,value|
-        ENV[key.to_s] = value
-    end if File.exists?(env_file)
-end
+CONFIG = YAML.load(File.read(File.expand_path('../application.yml',__FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env,{})
+CONFIG.symbolize_keys!
 
 module GAN
   class Application < Rails::Application
